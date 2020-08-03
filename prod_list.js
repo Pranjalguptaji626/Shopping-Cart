a61410ec4fa4943d6bc7b72028d7656c273ce446
Pranjal Gupta <pranjalguptaji626@gmail.com>
2020-08-03 17:21:41 +0530

    first commit

---
+++ prod_list.js
@@ -0,0 +1,32 @@
+product_list = {
+    products: [
+        {
+            image: "./images/iphone11.jpeg",
+            name: "Iphone 11",
+            curr_price: "50,000",
+            actual_price: "100,000",
+            discount: "50% OFF"
+        },
+        {
+            image: "./images/shoe.jpg",
+            name: "Nike shoes",
+            curr_price: "5,000",
+            actual_price: "12,000",
+            discount: "60% OFF"
+        },
+        {
+            image: "./images/slipper.jpg",
+            name: "Bata slippers",
+            curr_price: "500",
+            actual_price: "",
+            discount: ""
+        },
+        {
+            image: "./images/gucci_bag.jpg",
+            name: "School bag",
+            curr_price: "1500",
+            actual_price: "2000",
+            discount: "10% OFF"
+        }
+    ]
+}
